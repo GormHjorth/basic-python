e3d8d0fbc9defb7dd41f9861623ef56b4a26f333
GormHjorth <gorm.hermansen2@gmail.com>
2023-09-08 10:16:46 +0200

    hex1.2

--- src/hex.py
+++ src/hex.py
@@ -13,16 +13,21 @@ command, x = sys.argv[1:3]
 match command:
     case "encode":
         # Implement the encoding here
-        x = " ".join(x)
-        x = hex(ord(x))
-        encoding = x
+        encoding = ""
+        for i in x:
+            i = "".join(i)
+            i = hex(ord(i))
+            encoding = encoding + i
         print(encoding)
 
     case "decode":
         # Implement the decoding here
+        decoding = ""
         x = x.split("0x")
-        x = int(n, base = 16)
-        x = chr(x)
-        decoding = x
+        for i in x[1:]: 
+            print(i)
+            i = int(i, base = 16)
+            i = chr(i)
+            decoding = decoding + i
         print(decoding)
 
